7a9d9dd500002c6b9e56f4491fe64fbb615b2fc0
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-06 01:00:19 +0530

    Update Transaction Color

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className="minus">
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {sign}&#8377;{Math.abs(transaction.amount)}
