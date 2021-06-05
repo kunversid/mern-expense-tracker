575bf8b8297964ac15a42d84988278349b1bff4c
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-06 00:24:56 +0530

    Adding Context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,29 @@
+import React, { useContext, useReducer } from "react";
+
+//Initial State
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+//Create Context
+export const GlobalContext = useContext(initialState);
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
