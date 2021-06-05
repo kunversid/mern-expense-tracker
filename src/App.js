bbbde496d103b20b20e78613f0d24022ad1bdb1b
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-05 22:56:56 +0530

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
