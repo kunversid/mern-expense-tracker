71a04c0269f0012cd8c9245c0c2fe8087e2418d6
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-06 16:29:30 +0530

    Basic Routes

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const morgan = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
 
+const transactions = require("./routes/transactions");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
