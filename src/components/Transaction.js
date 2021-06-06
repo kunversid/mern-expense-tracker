ee9255dc3a0805e965f12f9980886916d651d874
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-06 15:45:38 +0530

    Deleting Transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
@@ -8,7 +9,12 @@ export const Transaction = ({ transaction }) => {
       <span>
         {sign}&#8377;{Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,10 +18,19 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
